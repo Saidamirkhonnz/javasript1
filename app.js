5405b1f9bada07662078ce4aa60de2d17746efcf
said <322@.ru>
2023-09-14 18:31:47 +0500

    java

---
+++ app.js
@@ -0,0 +1,37 @@
+
+var myWord = prompt('Введите свое имя')
+
+console.log('меня зовут ' + myWord);
+
+var age = prompt('Введите свой возраст')
+
+console.log('мне ' + age);
+
+var one = prompt('Решите пример 6 + 3 = ?')
+
+console.log('Пример 1: 6 + 3 = 9 Ваш ответ ' + one);
+
+var two = prompt('Решите пример 20 - 5 = ?')
+
+console.log('Пример 2: 20 - 5 = 15 Ваш ответ ' + two);
+
+var three = prompt('Решите пример 15 * 3 = ?')
+
+console.log('Пример 3: 15 * 3 = 45 Ваш ответ ' + three);
+
+var four = prompt('Решите пример 20 / 4 = ?')
+
+console.log('Пример 4: 20 / 4 = 5 Ваш ответ ' + four);
+
+var five = prompt('Решите пример c остатком 13 % 5 = ?')
+
+console.log('Пример 5: 13 % 5 = 3 Ваш ответ ' + five); 
+alert('Откройте консоль для просмотра')
+
+var X,Y,Z,res;
+X = prompt('Введите любое число')
+Y = prompt('Введите любое число')
+Z = prompt('Введите любое число')
+
+res = (X + Y + Z)/3
+console.log('Среднее арифметическое: '+ res)
